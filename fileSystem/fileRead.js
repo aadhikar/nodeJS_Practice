24a4118e390951a7f94e71dd225549e5de3e1f59
Anilkumar Adhikari <aadhikar@users.noreply.github.com>
2019-10-05 16:20:45 -0500

    new files added

--- fileSystem/fileRead.js
+++ fileSystem/fileRead.js
@@ -1,14 +1,13 @@
 var fs = require("fs");
 
 console.log("File read starts");
-var dataAsync = fs.readFile("./file.txt", "utf8", function(err, data){
-	if(err){
-		console.log(err);
-	}
+var dataAsync = fs.readFile(__dirname+"\\file.txt", "utf8", (err, data) => {
+	if(err) console.log(err);
 	console.log(data);
+	console.log("Asynch file read ends");
 });
 
-var dataSync = fs.readFileSync("./file1.txt", "utf8");
+var dataSync = fs.readFileSync(__dirname+"\\file1.txt", "utf8");
 console.log(dataSync);
 console.log("File read ends");
 
---
+++ index.js
@@ -0,0 +1,9 @@
+let Person = require('./person');
+let fileRead = require('./fileSystem/fileRead');
+
+// File System
+fileRead.dataAsync;
+fileRead.dataAsync;
+
+let person1 = new Person('Anilkumar', 30);
+person1.greeting();
